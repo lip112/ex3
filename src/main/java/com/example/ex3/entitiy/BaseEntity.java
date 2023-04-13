7e4ab197302d6ed3cbc9acbb5f1cf9b0ae9db830
aaaa <whaud1597@naver.com>
2023-04-13 09:43:25 +0900

    149 동적쿼리 까지

--- src/main/java/com/example/ex3/entitiy/BaseEntity.java
+++ src/main/java/com/example/ex3/entitiy/BaseEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass//해당 어노테이션이 적용된 클래스는 테이블로 생성되지 않는다. 하지만 상속받은 테이블은 생성된다. 중복제거
-@EntityListeners(value = { AuditingEntityListener.class})  //JPA내부에서 엔티티객체가 생성/변경되는것을 감지
+@EntityListeners(value = {AuditingEntityListener.class})  //JPA내부에서 엔티티객체가 생성/변경되는것을 감지
 @Getter
 abstract class BaseEntity {
     @CreatedDate
@@ -21,4 +21,4 @@ abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "moddate")
     private LocalDateTime modDate;
-    }
+}
--- src/main/java/com/example/ex3/repository/GuestbookRepository.java
+++ src/main/java/com/example/ex3/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package com.example.ex3.repository;
 
 import com.example.ex3.entitiy.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
